039a64d66dca3d5c9d2357d63fd06cfceafff21d
Luca Stefani <luca020400@lineageos.org>
2017-08-18 11:39:28 +0200

    thea: init: Update for O

--- init/init_thea.cpp
+++ init/init_thea.cpp
@@ -32,10 +32,11 @@
 #define _REALLY_INCLUDE_SYS__SYSTEM_PROPERTIES_H_
 #include <sys/_system_properties.h>
 
+#include <android-base/properties.h>
 #include "vendor_init.h"
 #include "property_service.h"
-#include "log.h"
-#include "util.h"
+
+using android::base::GetProperty;
 
 void gsm_properties(bool msim);
 void cdma_properties(const char *cdma_sub, const char *network);
@@ -53,13 +54,11 @@ void property_override(char const prop[], char const value[])
 
 void vendor_load_properties()
 {
-    int rc;
-
-    std::string platform = property_get("ro.board.platform");
+    std::string platform = GetProperty("ro.board.platform", "");
     if (platform != ANDROID_TARGET)
         return;
 
-    std::string radio = property_get("ro.boot.radio");
+    std::string radio = GetProperty("ro.boot.radio", "");
 
     property_override("ro.product.model", "Moto G 2014 LTE");
 
@@ -96,9 +95,6 @@ void vendor_load_properties()
         property_set("ro.mot.build.customerid", "retcn");
         property_override("ro.product.device", "thea_ds");
     }
-
-    std::string device = property_get("ro.product.device");
-    INFO("Found radio id %s setting build properties for %s device\n", radio.c_str(), device.c_str());
 }
 
 void gsm_properties(bool msim)
