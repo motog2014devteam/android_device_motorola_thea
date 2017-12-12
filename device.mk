327875708dc29964dc1118f35a6923ab8c46f4b2
Bubble <314877984@qq.com>
2017-12-12 14:48:26 +0800

    thea :Disable debug information

--- device.mk
+++ device.mk
@@ -37,5 +37,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/wifi/WCNSS_qcom_wlan_nv.bin:system/etc/firmware/wlan/prima/WCNSS_qcom_wlan_nv.bin
 
+# Bionic
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.bionic.ld.warning=0
+
 # Inherit from msm8226-common
 $(call inherit-product, device/motorola/msm8226-common/msm8226.mk)
